35b8f1cc4163847c00092cd16da6b8b3e2198a8f
Daniel Lee <dsl8va@gmail.com>
2021-01-18 16:47:25 -0500

    solution code

--- countUniqueValues.js
+++ countUniqueValues.js
@@ -38,3 +38,15 @@ function countUniqueValues(array) {
 }
 
 console.log(countUniqueValues([-2, -1, -1, 0, 1]))
+
+function countUniqueValues(arr) {
+  if (arr.length === 0) return 0;
+  let i = 0;
+  for (var j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+}
---
+++ maxSubarraySum.js
@@ -0,0 +1,22 @@
+/*
+Write a function called maxSubarraySum which accepts an array of integers and a number called n. The function hsould calculate the maximum sum of n consecutive elements in the array.
+
+maxSubarraySum([4,2,1,6], 1) // 6
+maxSubarraySum([1,2,5,2,8,1,5], 2) // 10
+*/
+
+function maxSubarraySum(arr, num) {
+  var maxSum = 0, tempSum = 0;
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+  tempSum = maxSum;
+
+  for (let j = num; j < (arr.length - num + 1); j++) {
+    tempSum = tempSum - arr[j-num] + arr[j];
+    maxSum = Math.max(tempSum, maxSum)
+  }
+  return maxSum;
+}
+
+console.log(maxSubarraySum([1,2,5,2,8,1,5], 2))
