71d2be2bb8162dcdb171ec3eb2c6407d70fe5184
Daniel Lee <dsl8va@gmail.com>
2021-01-22 16:36:08 -0500

    mergeSort

--- sortingAlgo.js
+++ sortingAlgo.js
@@ -20,7 +20,7 @@ function bubbleSort(arr) {
   return arr;
 }
 
-bubbleSort([5,3,6,1])
+// bubbleSort([5,3,6,1])
 
 function selectionSort(arr) {
 
@@ -29,9 +29,63 @@ function selectionSort(arr) {
     for (let j = i+1; j < arr.length; j++) {
       if (arr[j] < arr[lowest]) lowest = j;
     }
-    swap(arr, i, lowest)
+    if (lowest !== i) swap(arr, i, lowest);
   }
   return arr
 }
-debugger;
-selectionSort([5,3,6,1])
+// debugger;
+// selectionSort([5,3,6,1])
+
+function insertionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let pointer = i + 1;
+    if (arr[pointer] < arr[i]) {
+      for (let j = i; j >= 0; j--) {
+        if (arr[pointer] < arr[j]) {
+        swap(arr, j, pointer)
+        pointer--;
+        console.log(arr)
+        }
+      }
+    }
+  }
+  return arr;
+}
+
+// insertionSort([5,3,6,2,1])
+
+function combine(arr1, arr2) {
+  let arr = [];
+  let i = 0, j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] <= arr2[j]) {
+      arr.push(arr1[i]);
+      i++;
+    } else {
+      arr.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    arr.push(arr1[i]);
+    i++
+  }
+  while (j < arr2.length) {
+    arr.push(arr2[j]);
+    j++
+  }
+  return arr;
+}
+// combine([1,3,5], [2,4,6])
+
+function mergeSort(arr) {
+   if (arr.length <= 1) return arr;
+
+   let mid = Math.floor(arr.length/2);
+   let left = mergeSort(arr.slice(0, mid));
+   let right = mergeSort(arr.slice(mid));
+   return combine(left, right);
+
+}
+
+console.log(mergeSort([5,2,6,1,4,5]))
